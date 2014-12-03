1547d1202d49cd4f1a54d3ee8938afec62fef676
RossLitz <codingclasses.049@gmail.com>
2014-12-03 15:07:44 -0600

    all the code will be contained in the pubic folder

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-	response.write('Hello world');
-	response.end();
-});
-
-// app.get('/blocks', function(request, response){
-// 	var blocks = ['Fixed', 'Movable', 'Rotating'];
-// 	response.json(blocks);
+// app.get('/', function(request, response){
+// 	response.sendFile(__dirname + '/public/index.html');
 // });
-
+// static middleware serving files form the public folder
+app.use(express.static('public'));
 app.get('/blocks', function(request, response){
-	response.redirect(301,'/parts');
+	var blocks = ['Fixed', 'Movable', 'Rotating'];
+	response.json(blocks);
 });
+
 app.listen(3000, function(){
 	console.log('Listening on port 3000');
 });
